483f78a0d43f3130106fa75021ab59579bdae793
anna@sk.com <anna@sk.com>
2025-08-08 01:00:46 +0900

    init: first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactEditor from './components/ReactEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactEditor />
+    </div>
+  );
+}
+
+export default App;
